a1171ec4aee8b984edd1bea0ca0e844fe666f734
sadhanids <sadhanids@gmail.com>
2025-08-20 06:55:44 +0300

    I have completed module 2,task 3 activities.

---
+++ Module2/task03.py
@@ -0,0 +1,7 @@
+Length_of_rectangle = float(input("Enter the length of the rectangle: "))
+Width_of_rectangle = float(input("Enter the width of the rectangle: "))
+Area_of_a_rectangle = Length_of_rectangle * Width_of_rectangle
+print(f"The area of the rectangle is: {Area_of_a_rectangle}")
+
+perimeter_of_rectangle = 2*(Length_of_rectangle + Width_of_rectangle)
+print(f"The perimeter of the rectangle is: {perimeter_of_rectangle}")
