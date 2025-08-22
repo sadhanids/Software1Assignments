17070fd28d84c72c231bbb1ef2257710ce3ac8dd
sadhanids <sadhanids@gmail.com>
2025-08-22 14:03:53 +0300

    I have completed module 2,task 6 activity.

---
+++ Module2/task06.py
@@ -0,0 +1,12 @@
+import random
+
+Random_Number1 = random.randint(0,9)
+Random_Number2 = random.randint(0,9)
+Random_Number3 = random.randint(0,9)
+print(f"3-digit code: {str(Random_Number1) + str(Random_Number2) + str(Random_Number3)}")
+
+Random_Number1 = random.randint(1,6)
+Random_Number2 = random.randint(1,6)
+Random_Number3 = random.randint(1,6)
+Random_Number4 = random.randint(1,6)
+print(f"4-digit code: {Random_Number1}{Random_Number2}{Random_Number3}{Random_Number4}")
