915c1f9398823e4823c4c84988ae451847237f56
sadhanids <sadhanids@gmail.com>
2025-08-29 23:07:29 +0300

    I have completed module 5,task 1 activity.

--- Module3/task01.py
+++ Module3/task01.py
@@ -1,1 +1,9 @@
-print("Hello World")
+length_of_zander = int(input("Enter the length of zander in centimeters: "))
+standard_length_of_zander = 42
+deviation = standard_length_of_zander - length_of_zander
+if length_of_zander < 42:
+    print(f"Release the fish back into the lake, {deviation}cm below the standard size of zander ")
+else:
+    print("The fish meets the legal size of zander, you can keep the fish!")
+
+
