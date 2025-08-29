28ebfb325f227aa71eecbdad14a9436014be552d
sadhanids <sadhanids@gmail.com>
2025-08-29 22:40:10 +0300

    I have completed module 4,task 6 activity.

--- Module4/task06.py
+++ Module4/task06.py
@@ -2,15 +2,17 @@ import random
 
 N = int(input("Enter the number of random points to generate: "))
 points_inside_circle = 0
-for i in range(N):
+total_points = 0
+
+while total_points < N:
     x=random.uniform(-1,1)
     y=random.uniform(-1,1)
     if x**2 + y**2 < 1:
         points_inside_circle += 1
 
     pi_estimate = 4 * points_inside_circle / N
+    total_points += 1
 
 print(f"Approximation of pi:{pi_estimate:.2f}")
-
 
 
