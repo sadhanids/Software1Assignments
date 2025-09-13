d30cfa3412dcacb2a1f7074052e2a5684117c361
sadhanids <sadhanids@gmail.com>
2025-09-14 00:49:34 +0300

    I have completed module 8,task 2 activity.

--- Module8/task02.py
+++ Module8/task02.py
@@ -1,1 +1,25 @@
 
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='DohaLife12*',
+    autocommit=True
+)
+
+def search_for_country(area_code):
+    sql = f"SELECT type, COUNT(*) AS airport_count FROM airport WHERE iso_country = 'FI' GROUP BY type ORDER BY type;"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    database_data = cursor.fetchall()
+    if len(database_data) > 0:
+        for data in database_data:
+            print(f" Airport Type: {data[0]}")
+            print(f" Airport Count: {data[1]}")
+
+
+user_input = input("Enter iso_country code: ")
+search_for_country(user_input)
